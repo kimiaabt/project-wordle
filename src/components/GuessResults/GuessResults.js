5fa9babc1073829df3124331cf76d15d500e6d0e
Kimia Abtahi <kimia.abtahi@bibsent.no>
2023-10-11 13:45:03 +0200

    remove console log

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -4,7 +4,6 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ results }) {
-  console.log("Results: ", results);
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
