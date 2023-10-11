17aacbc33124a3104147bae08e0b386acdba1334
Kimia Abtahi <kimia.abtahi@bibsent.no>
2023-10-11 16:43:29 +0200

    nvm I changed my mind over the renaming of Banner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,7 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
-import GameOverBanner, { Status } from "../Banner/Banner";
+import Banner, { Status } from "../Banner/Banner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -26,17 +26,17 @@ function Game() {
         gameStatus={gameStatus}
       />
       {gameStatus === Status.WON && (
-        <GameOverBanner status="happy">
+        <Banner status="happy">
           <strong>Congratulations! </strong>
           You got it in {guesses.length}{" "}
           {guesses.length === 1 ? "guess" : "guesses"}.
-        </GameOverBanner>
+        </Banner>
       )}
       {gameStatus === Status.LOST && (
-        <GameOverBanner status="sad">
+        <Banner status="sad">
           Sorry, the correct answer was
           <strong> {answer}.</strong>
-        </GameOverBanner>
+        </Banner>
       )}
     </>
   );
